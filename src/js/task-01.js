91944dee37b5523da3de7076acb1a12d8c0c29da
Prykhodko-Pasha <prykhodko.p@gmail.com>
2021-07-01 17:39:37 +0300

    update task-01

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -3,8 +3,7 @@ console.log(`В списке ${itemsElQuantity} категории.`);
 
 const itemsElArray = document.querySelectorAll(".item");
 // console.log(itemsElArray);
-for (const item of itemsElArray) {
-  console.log("Категория: ", item.firstElementChild.firstChild.textContent);
-  //   console.log("Количество элементов: ", item.children[1].children.length);
+itemsElArray.forEach((item) => {
+  console.log("Категория: ", item.firstElementChild.textContent);
   console.log("Количество элементов: ", item.querySelectorAll("li").length);
-}
+});
